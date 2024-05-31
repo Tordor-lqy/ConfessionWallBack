b3d8c7722bc5f3ada8bab5c43a9dce229a2415fc
Tordor <3262978839@qq.com>
2024-06-01 00:52:05 +0800

    init

---
+++ src/main/java/com/sanding/confessionwallback/ConfessionWallBackApplication.java
@@ -0,0 +1,12 @@
+package com.sanding.confessionwallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfessionWallBackApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfessionWallBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sanding/confessionwallback/ConfessionWallBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanding.confessionwallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfessionWallBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
