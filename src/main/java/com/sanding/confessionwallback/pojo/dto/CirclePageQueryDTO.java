2b4ce0d108b0c4d64b808b615f33da93a3cd3342
Tordor <3262978839@qq.com>
2024-06-06 13:20:13 +0800

    代码合并

---
+++ src/main/java/com/sanding/confessionwallback/pojo/dto/CirclePageQueryDTO.java
@@ -0,0 +1,20 @@
+package com.sanding.confessionwallback.pojo.dto;
+
+import io.swagger.models.auth.In;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class CirclePageQueryDTO implements Serializable {
+    //页数
+    private Integer p;
+    //每页条数
+    private Integer s;
+    //圈子名称
+    private String circleName;
+    //圈子类别
+    private String circleType;
+
+
+}
