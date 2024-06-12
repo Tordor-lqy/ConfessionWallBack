82f52ab6f2e7df99d30f2bccfc7d1c1a20b31a95
985Mingg <3443539986@qq.com>
2024-06-12 11:37:05 +0800

    用户新建话题

--- src/main/java/com/sanding/confessionwallback/controller/user/UserTopicController.java
+++ src/main/java/com/sanding/confessionwallback/controller/user/UserTopicController.java
@@ -2,13 +2,12 @@ package com.sanding.confessionwallback.controller.user;
 
 import com.sanding.confessionwallback.common.result.PageResult;
 import com.sanding.confessionwallback.common.result.Result;
+import com.sanding.confessionwallback.pojo.dto.TopicDTO;
 import com.sanding.confessionwallback.pojo.dto.TopicPageQueryDTO;
 import com.sanding.confessionwallback.service.TopicService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user/topic")
@@ -19,6 +18,11 @@ public class UserTopicController {
     @Autowired
     private TopicService topicService;
 
+    /**
+     * 分页查询某分组话题
+     * @param topicPageQueryDTO
+     * @return
+     */
     @GetMapping
     public Result<PageResult> pageByGroup(TopicPageQueryDTO  topicPageQueryDTO){
         log.info("分页查询某分组话题：{}",topicPageQueryDTO);
@@ -26,4 +30,15 @@ public class UserTopicController {
         return  Result.success(pageResult);
     }
 
+    /**
+     * 新建话题
+     * @param topicDTO
+     * @return
+     */
+    @PostMapping
+    public Result<TopicDTO> save(@RequestBody TopicDTO topicDTO){
+        log.info("新建话题:{}",topicDTO);
+        topicService.save(topicDTO);
+        return Result.success();
+    }
 }
--- src/main/java/com/sanding/confessionwallback/service/impl/TopicServiceImpl.java
+++ src/main/java/com/sanding/confessionwallback/service/impl/TopicServiceImpl.java
@@ -3,21 +3,31 @@ package com.sanding.confessionwallback.service.impl;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.sanding.confessionwallback.common.exception.TopicException;
 import com.sanding.confessionwallback.common.result.PageResult;
 import com.sanding.confessionwallback.mapper.TopicMapper;
+import com.sanding.confessionwallback.pojo.dto.TopicDTO;
 import com.sanding.confessionwallback.pojo.dto.TopicPageQueryDTO;
 import com.sanding.confessionwallback.pojo.entity.Group;
 import com.sanding.confessionwallback.pojo.entity.Post;
 import com.sanding.confessionwallback.pojo.entity.Topic;
 import com.sanding.confessionwallback.service.TopicService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TopicServiceImpl implements TopicService {
     @Autowired
     private TopicMapper topicMapper;
 
+    /**
+     * 分页查询某组话题
+     * @param topicPageQueryDTO
+     * @return
+     */
     @Override
     public PageResult pageByGroup(TopicPageQueryDTO topicPageQueryDTO) {
         Page<Topic> page =new Page<Topic>(topicPageQueryDTO.getP(),topicPageQueryDTO.getS());
@@ -28,5 +38,28 @@ public class TopicServiceImpl implements TopicService {
         Page<Topic> resultPage = topicMapper.selectPage(page,wrapper);
 
         return new PageResult(resultPage.getTotal(),resultPage.getRecords());
+    }
+
+    /**
+     * 新建话题
+     * @param topicDTO
+     */
+    @Override
+    public void save(TopicDTO topicDTO) {
+        //判断话题是否存在
+        LambdaQueryWrapper<Topic> wrapper =new LambdaQueryWrapper<Topic>()
+                .select(Topic::getTopicId)
+                .eq(Topic::getTopicName,topicDTO.getTopicName())
+                .eq(Topic::getCircleId,topicDTO.getCircleId())
+                .eq(Topic::getGroupId,topicDTO.getGroupId());
+        List<Long> topicId = topicMapper.selectObjs(wrapper);
+        if(topicId.size()>0){
+            throw new TopicException("该话题已存在");
+        }else {
+            //创建新话题
+            Topic tp=new Topic();
+            BeanUtils.copyProperties(topicDTO,tp);
+            topicMapper.insert(tp);
+        }
     }
 }
