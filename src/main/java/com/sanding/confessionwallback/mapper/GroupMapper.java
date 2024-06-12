ad4a7a79d6a3eb575cb1c30b0302358abf192bf1
Tordor <3262978839@qq.com>
2024-06-12 13:24:57 +0800

    新增分组Mapper

---
+++ src/main/java/com/sanding/confessionwallback/mapper/GroupMapper.java
@@ -0,0 +1,7 @@
+package com.sanding.confessionwallback.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sanding.confessionwallback.pojo.entity.Group;
+
+public interface GroupMapper extends BaseMapper<Group> {
+}
