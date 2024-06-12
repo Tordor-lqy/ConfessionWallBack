a8f75bcc4c08b90a218371c6127d4947e7facf9c
985Mingg <3443539986@qq.com>
2024-06-12 11:38:08 +0800

    漏了一个异常类

---
+++ src/main/java/com/sanding/confessionwallback/common/exception/TopicException.java
@@ -0,0 +1,14 @@
+package com.sanding.confessionwallback.common.exception;
+
+/**
+ * 关于话题的异常
+ */
+public class TopicException extends BaseException{
+
+    public TopicException(){
+
+    }
+    public TopicException(String msg){
+        super(msg);
+    }
+}
